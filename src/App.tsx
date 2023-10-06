7695fb9345418b170e2382cdda23dfd69a34f805
PabloAllves <ribeirojr1522@gmail.com>
2023-10-06 09:32:58 -0300

    :feat: footer concluido

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import RoutesApp from "./routes/routesApp";
+import { Footer } from "./components/interface/footer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <RoutesApp />
+        <Footer />
       </BrowserRouter>
     </>
   );
---
+++ src/components/interface/footer/index.tsx
@@ -0,0 +1,50 @@
+import styles from "./footer.module.css";
+import {
+  FaInstagram,
+  FaYoutube,
+  FaWhatsapp,
+  FaFacebookF,
+} from "react-icons/fa";
+
+import Logo from "../../../assets/logo.png";
+import { whats } from "../../../config/links";
+import { Link } from "react-router-dom";
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <section>
+        <a
+          href="https://www.instagram.com/abelkgroup/"
+          target="_blank"
+          rel="external"
+        >
+          <span>
+            <FaInstagram />
+          </span>
+        </a>
+        {/* <a href="#" target="_blank" rel="external">
+          <span>
+            <FaYoutube />
+          </span>
+        </a> */}
+        <a href={whats} target="_blank" rel="external">
+          <span>
+            <FaWhatsapp />
+          </span>
+        </a>
+        {/* <a href="#" target="_blank" rel="external">
+          <span>
+            <FaFacebookF />
+          </span>
+        </a> */}
+      </section>
+      <section className={styles.info}>
+        <p>R. Brasília, 272 - Carlos Prates - Belo Horizonte - MG, 30710-270</p>
+        <Link to="/">
+          <img src={Logo} alt="Logo Abelk Capital" width={170} height={40} />
+        </Link>
+      </section>
+    </footer>
+  );
+}
